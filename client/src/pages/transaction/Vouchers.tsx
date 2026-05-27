711ab99bf76b6df16e2f4983ab0ea6233656915d
darshankochar22 <darshannn022@gmail.com>
2026-05-27 21:06:48 +0530

    transaction setup

---
+++ client/src/pages/transaction/Vouchers.tsx
@@ -0,0 +1,8 @@
+
+export default function Voucher(){
+    return(
+        <>
+        Voucher
+        </>
+    );
+}
---
+++ client/src/pages/transaction/ui/index.ts
@@ -0,0 +1,2 @@
+
+export { default as AmountDisplay, formatINR } from "./AmountDisplay";
